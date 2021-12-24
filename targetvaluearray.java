9448bee5b0599460017d90fe5ae9785d52d73b9a
MonikaMS17 <94360430+MonikaMS17@users.noreply.github.com>
2021-12-24 21:01:36 +0530

    Add files via upload

---
+++ targetvaluearray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class targetvaluearray{
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n= sc.nextInt();
+            int marks[]=new int[n];
+            for(int i=0;i<n;i++){
+                marks[i]=sc.nextInt();
+            }
+            int target= sc.nextInt();
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(marks[j]==target){
+                    count=count+1;
+                }
+            }
+            if(count>0){
+                System.out.println("the target is "+count+" times present in the array");
+            }
+            else{
+                System.out.println("target value is not present in the array");
+            }
+        }
+    }
+}
